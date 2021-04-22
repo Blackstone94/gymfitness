b100c5f5109e9da3e93d864408264ee934972ef3
Edgar Ortega <eroa94@gmail.com>
2021-04-22 11:02:22 -0500

    estilos encabezados

--- wp-content/themes/gymfitness/functions.php
+++ wp-content/themes/gymfitness/functions.php
@@ -11,7 +11,7 @@
     function gymfitness_scripts_styles(){
 
         wp_enqueue_style('normalize',get_template_directory_uri( ).'/css/normalize.css',array(),'8.0.1');
-        wp_enqueue_style('googleFont','<link rel="preconnect" href="https://fonts.gstatic.com"> <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&family=Raleway:wght@400;700;900&family=Staatliches&display=swap" rel="stylesheet">',array(),'1.0.0');
+        wp_enqueue_style('googleFont', 'https://fonts.googleapis.com/css?family=Open+Sans|Raleway:400,700,900|Staatliches&display=swap', array(), null );
 
         //la hoja de estilos principal
         wp_enqueue_style('style', get_stylesheet_uri(),array('normalize','googleFont'), '1.0.0');   
