2b1b69d4eae07935d286b95bd9f003881e82fabe
Edgar Ortega <eroa94@gmail.com>
2021-04-23 10:31:52 -0500

    agregando menu  movil

--- wp-content/themes/gymfitness/functions.php
+++ wp-content/themes/gymfitness/functions.php
@@ -13,8 +13,14 @@
         wp_enqueue_style('normalize',get_template_directory_uri( ).'/css/normalize.css',array(),'8.0.1');
         wp_enqueue_style('googleFont', 'https://fonts.googleapis.com/css?family=Open+Sans|Raleway:400,700,900|Staatliches&display=swap', array(), null );
 
+        wp_enqueue_style('slickNavCss',get_template_directory_uri( ).'/css/slicknav.min.css',array(),'1.0.10');
+
         //la hoja de estilos principal
         wp_enqueue_style('style', get_stylesheet_uri(),array('normalize','googleFont'), '1.0.0');   
+
+        wp_enqueue_script('slickNavJs',get_template_directory_uri( ).'/js/jquery.slicknav.min.js',array('jquery'),'1.0.10');
+        wp_enqueue_script('scripts',get_template_directory_uri( ).'/js/scripts.js',array('jquery','slickNavJs'),'1.0.0');
+
     }
     add_action( 'wp_enqueue_scripts','gymfitness_scripts_styles');
 
---
+++ wp-content/themes/gymfitness/js/scripts.js
@@ -0,0 +1,3 @@
+jQuery(document).ready($ => {
+    $('.menu-principal ul').slicknav();
+});
