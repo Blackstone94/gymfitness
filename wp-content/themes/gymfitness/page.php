2d5d223ef2a9854d5850bc21e813985a12ec7d53
Edgar Ortega <eroa94@gmail.com>
2021-04-26 14:28:24 -0500

    templates

--- wp-content/themes/gymfitness/page.php
+++ wp-content/themes/gymfitness/page.php
@@ -1,19 +1,8 @@
 <?php get_header(); ?>
     <main class="contenedor seccion pagina con-sidebar">
         <div class="contenido-principal">
-            <?php while(have_posts()):the_post();?>
-            <h1 class="texto-centrado texto-primario"> <?php the_title();?></h1>
-            <?php 
-            if(has_post_thumbnail()):
-                the_post_thumbnail('blog', array(
-                    'class'=>'imagen-destacada'
-                ));
-            endif; ?>
-            <?php the_content();  
-            endwhile;?>
+            <?php get_template_part('templates-parts/paginas');?>
         </div>
-
         <?php get_sidebar();?>
     </main>
-
     <?php get_footer();?>
