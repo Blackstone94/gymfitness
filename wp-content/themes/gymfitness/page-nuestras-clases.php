ef9c3ab4680627518eb2776130cdade0369a3465
Edgar Ortega <eroa94@gmail.com>
2021-04-29 14:22:29 -0500

    63

--- wp-content/themes/gymfitness/page-nuestras-clases.php
+++ wp-content/themes/gymfitness/page-nuestras-clases.php
@@ -1,7 +1,8 @@
 <?php get_header(); ?>
     <main class="contenedor seccion pagina no-sidebar">
-        <div class="contenido-principal">
+        <div class="texto-centrado">
             <?php get_template_part('templates-parts/paginas');?>
+
             <?php gymfitness_lista_clases(); ?>
         </div>
     </main>
