4d98411393e7583aaca3eff972b36d9b4bab42d4
Edgar Ortega <eroa94@gmail.com>
2021-05-07 15:50:05 -0500

    add section

--- wp-content/themes/gymfitness/front-page.php
+++ wp-content/themes/gymfitness/front-page.php
@@ -1,2 +1,6 @@
 <?php get_header('front');?>
+    <section class="texto-centrado seccion contenedor bienvenida">
+        <h2 class="texto-primario"><?php the_field('encabezado')?></h2>
+        <p ><?php the_field('texto_bienvenida');?></p>
+    </section>
 <?php get_footer();?>
